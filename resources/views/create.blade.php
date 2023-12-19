ba9e9abbad0b8b14168508d15603446479986070
Janis <janisjekelis@gmail.com>
2023-12-19 11:39:02 +0200

    ac

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -10,8 +10,25 @@
 <body>
 <form method="post" action={{route('store')}}>
     @csrf
-<input type="text" name="PIN">
+    @method('PUT')
+    <label for="name">Name:</label>
+    <input type="text" name="name" id="name">
+    <label for="surname">Surname:</label>
+    <input type="text" name="surname" id="surname">
+    <label for="email">Email:</label>
+    <input type="text" name="email" id="email">
+    <label for="password">Password:</label>
+    <input type="password" name="password" id="password">
+    <label for="password-confirm">Confirm password:</label>
+    <input type="password" name="passwordConfirm" id="password-confirm">
+    <label for="currency"></label>
+    <select id="currency">
+        @foreach($currencies as $currency)
+            <option value={{$currency}}>{{$currency}}</option>
+        @endforeach
+    </select>
     <button type="submit">Register</button>
-</form>
+    </form>
+
 </body>
 </html>
