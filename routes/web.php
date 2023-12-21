079b68f4da3b8f6a768382e682a542d91395877d
Janis <janisjekelis@gmail.com>
2023-12-21 12:33:13 +0200

    ac

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,14 @@
 use App\Http\Controllers\DebitAccountController;
 use App\Http\Controllers\InvestmentAccountController;
 use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 //Route::get('/',[UserAccountController::class,'index']);
 Route::resource('/home',UserController::class);
 Route::resource('/home/{user}/debit',DebitAccountController::class);
 Route::resource('/home/{user}/invest',InvestmentAccountController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
